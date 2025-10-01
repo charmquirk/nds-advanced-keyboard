52cd9cd99fc90f7fe95636d07b09d63a7a35f315
charmquirk <charmquirk@protonmail.com>
2025-10-01 06:38:22 -0400

    Added custom texture
    TODO:
    - Fix proportions and pressed states.

---
+++ build/keyboard_custom/graphics/keyboardGfx.png.d
@@ -0,0 +1,2 @@
+build/keyboard_custom/graphics/keyboardGfx.png.o: \
+ build/keyboard_custom/graphics/keyboardGfx.c
--- source/main.c
+++ source/main.c
@@ -8,72 +8,61 @@
 
 #include "keyboardGfx.h"
 
+static const int keyDimensions = 20;
+
 // Default keyboard map
-static const s16 SimpleKbdLower[] =
+// Needs DVK_FOLD, DVK_MENU, DVK_LEFT, DVK_DOWN, DVK_RIGHT, DVK_UP
+static const s16 KbdLower[] =
 {
-    DVK_FOLD, DVK_FOLD, NOKEY, '1', '1', '2', '2', '3', '3', '4', '4', '5', '5',
-    '6', '6', '7', '7', '8', '8', '9', '9', '0', '0', '-', '-', '=', '=',
-    DVK_BACKSPACE, DVK_BACKSPACE, DVK_BACKSPACE, DVK_BACKSPACE, DVK_BACKSPACE,
+    '1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6',
+    '7', '7', '8', '8', '9', '9', '0', '0', '-', '-', '=', '=',
 
-    DVK_TAB, DVK_TAB, DVK_TAB, DVK_TAB, 'q', 'q', 'w', 'w', 'e', 'e', 'r', 'r',
-    't', 't', 'y', 'y', 'u', 'u', 'i', 'i', 'o', 'o', 'p', 'p', '[', '[', ']',
-    ']', '\\', '\\', '`', '`',
+    'q', 'q', 'w', 'w', 'e', 'e', 'r', 'r', 't', 't', 'y', 'y',
+    'u', 'u', 'i', 'i', 'o', 'o', 'p', 'p', '[', '[', ']', ']',
 
-    DVK_CAPS, DVK_CAPS, DVK_CAPS, DVK_CAPS, DVK_CAPS, 'a', 'a', 's', 's', 'd',
-    'd', 'f', 'f', 'g', 'g', 'h', 'h', 'j', 'j', 'k', 'k', 'l', 'l', ';', ';',
-    '\'', '\'', DVK_ENTER, DVK_ENTER, DVK_ENTER, DVK_ENTER, DVK_ENTER,
+    'a', 'a', 's', 's', 'd', 'd', 'f', 'f', 'g', 'g', 'h', 'h',
+    'j', 'j', 'k', 'k', 'l', 'l', ';', ';', '\'', '\'', '\\', '\\', 
 
-    DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, 'z', 'z',
-    'x', 'x', 'c', 'c', 'v', 'v', 'b', 'b', 'n', 'n', 'm', 'm', ',', ',', '.',
-    '.', '/', '/', NOKEY, NOKEY, DVK_UP, DVK_UP, NOKEY, NOKEY,
+    NOKEY, 'z', 'z', 'x', 'x', 'c', 'c', 'v', 'v', 'b', 'b', 'n',
+    'n', 'm', 'm', ',', ',', '.', '.', '/', '/', '`', '`', NOKEY,
 
-    DVK_CTRL, DVK_CTRL, DVK_CTRL, DVK_CTRL, DVK_CTRL, DVK_ALT, DVK_ALT, DVK_ALT,
-    DVK_ALT, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE,
-    DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_MENU,
-    DVK_MENU, DVK_MENU, DVK_MENU, DVK_MENU, DVK_LEFT, DVK_LEFT, DVK_DOWN,
-    DVK_DOWN, DVK_RIGHT, DVK_RIGHT
+    DVK_TAB, DVK_TAB, DVK_ENTER,DVK_ENTER, DVK_CAPS, DVK_CAPS, DVK_ALT, DVK_ALT,
+    DVK_CTRL, DVK_CTRL, DVK_SPACE, DVK_SPACE, DVK_BACKSPACE, DVK_BACKSPACE, NOKEY, NOKEY,
 };
 
-static const s16 SimpleKbdUpper[] =
+static const s16 KbdUpper[] =
 {
-    DVK_FOLD, DVK_FOLD, NOKEY, '!', '!', '@', '@', '#', '#', '$', '$', '%', '%',
-    '^', '^', '&', '&', '*', '*', '(', '(', ')', ')', '_', '_', '+', '+',
-    DVK_BACKSPACE, DVK_BACKSPACE, DVK_BACKSPACE, DVK_BACKSPACE, DVK_BACKSPACE,
+    '!', '!', '@', '@', '#', '#', '$', '$', '%', '%', '^', '^',
+    '&', '&', '*', '*', '(', '(', ')', ')', '_', '_', '+', '+',
 
-    DVK_TAB, DVK_TAB, DVK_TAB, DVK_TAB, 'Q', 'Q', 'W', 'W', 'E', 'E', 'R', 'R',
-    'T', 'T', 'Y', 'Y', 'U', 'U', 'I', 'I', 'O', 'O', 'P', 'P', '{', '{', '}',
-    '}', '|', '|', '~', '~',
+    'Q', 'Q', 'W', 'W', 'E', 'E', 'R', 'R', 'T', 'T', 'Y', 'Y',
+    'U', 'U', 'I', 'I', 'O', 'O', 'P', 'P', '{', '{', '}', '}',
 
-    DVK_CAPS, DVK_CAPS, DVK_CAPS, DVK_CAPS, DVK_CAPS, 'A', 'A', 'S', 'S', 'D',
-    'D', 'F', 'F', 'G', 'G', 'H', 'H', 'J', 'J', 'K', 'K', 'L', 'L', ':', ':',
-    '"', '"', DVK_ENTER, DVK_ENTER, DVK_ENTER, DVK_ENTER, DVK_ENTER,
+    'A', 'A', 'S', 'S', 'D', 'D', 'F', 'F', 'G', 'G', 'H', 'H',
+    'J', 'J', 'K', 'K', 'L', 'L', ':', ':', '"', '"', '|', '|',
 
-    DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, DVK_SHIFT, 'Z', 'Z',
-    'X', 'X', 'C', 'C', 'V', 'V', 'B', 'B', 'N', 'N', 'M', 'M', '<', '<', '>',
-    '>', '?', '?', NOKEY, NOKEY, DVK_UP, DVK_UP, NOKEY, NOKEY,
+    NOKEY, 'Z', 'Z', 'X', 'X', 'C', 'C', 'V', 'V', 'B', 'B', 'N',
+    'N', 'M', 'M', '<', '<', '>', '>', '?', '?', '~', '~', NOKEY,
 
-    DVK_CTRL, DVK_CTRL, DVK_CTRL, DVK_CTRL, DVK_CTRL, DVK_ALT, DVK_ALT, DVK_ALT,
-    DVK_ALT, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE,
-    DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_SPACE, DVK_MENU,
-    DVK_MENU, DVK_MENU, DVK_MENU, DVK_MENU, DVK_LEFT, DVK_LEFT, DVK_DOWN,
-    DVK_DOWN, DVK_RIGHT, DVK_RIGHT
+    DVK_TAB, DVK_TAB, DVK_ENTER,DVK_ENTER, DVK_CAPS, DVK_CAPS, DVK_ALT, DVK_ALT,
+    DVK_CTRL, DVK_CTRL, DVK_SPACE, DVK_SPACE, DVK_BACKSPACE, DVK_BACKSPACE, NOKEY, NOKEY,
 };
 
-static const KeyMap capsLock =
+static const KeyMap capsOn =
 {
-    .mapDataPressed = keyboardGfxMap + 32 * 20,
+    .mapDataPressed = keyboardGfxMap + keyDimensions * 2,
     .mapDataReleased = keyboardGfxMap,
-    .keymap = SimpleKbdUpper,
-    .width = 32,
+    .keymap = KbdUpper,
+    .width = 24,
     .height = 5
 };
 
-static const KeyMap lowerCase =
+static const KeyMap capsOff =
 {
-    .mapDataPressed = keyboardGfxMap + 32 * 30,
-    .mapDataReleased = keyboardGfxMap + 32 * 10,
-    .keymap = SimpleKbdLower,
-    .width = 32,
+    .mapDataPressed = keyboardGfxMap + keyDimensions * 4,
+    .mapDataReleased = keyboardGfxMap + keyDimensions * 3,
+    .keymap = KbdLower,
+    .width = 24,
     .height = 5
 };
 
@@ -81,8 +70,8 @@ static const Keyboard customKeyboard =
 {
     .scrollSpeed = 3,
 
-    .grid_width = 8,   // Grid width
-    .grid_height = 16, // Grid height
+    .grid_width = keyDimensions/2,   // Grid width
+    .grid_height = keyDimensions, // Grid height
 
     // By setting the initial state to uppercase, and marking the keyboard as
     // shifted, the first character will be uppercase and the next ones will be
@@ -91,8 +80,8 @@ static const Keyboard customKeyboard =
     .state = Upper,
 
     .mappings = {
-        &lowerCase, // keymap for lowercase
-        &capsLock,  // keymap for caps lock
+        &capsOff,   // keymap for lowercase
+        &capsOn,    // keymap for uppercase
         0,          // keymap for numeric entry
         0           // keymap for reduced footprint
     },
